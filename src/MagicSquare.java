9486e8e1f34b13a8e73665345468dab37e634ffd
angelashi <angelas2026@student.cis.edu.hk>
2024-04-10 14:07:32 +0800

    quiz

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -6,7 +6,11 @@ public class MagicSquare {
      * @return the sum of the first row in array2d.
      */
     public static int calculateCheckSum(int[][] array2d) {
-
+        int sum = 0;
+        for (int i = 0; i < array2d[0].length; i++){
+            sum += array2d[0][i];
+        }
+        return sum;
     }
 
     /**
@@ -19,7 +23,17 @@ public class MagicSquare {
      *         iterating from the second row.
      */
     public static boolean magicRows(int[][] array2d, int checkSum) {
-
+        checkSum = calculateCheckSum(array2d);
+        for (int i = 0; i < array2d.length; i++){
+            int sum = 0;
+            for (int j = 0; j < array2d[i].length; j++){
+                sum += array2d[i][j];
+            }
+            if (sum != checkSum){
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -29,7 +43,17 @@ public class MagicSquare {
      * @return whether the sum of each column in array2d is equal to checkSum or not.
      */
     public static boolean magicColumns(int[][] array2d, int checkSum) {
-
+        checkSum = calculateCheckSum(array2d);
+        for (int j = 0; j < array2d[0].length; j++){
+            int sum = 0;
+            for (int i = 0; i < array2d.length; i++){
+                sum += array2d[i][j];
+            }
+            if (sum != checkSum){
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -39,7 +63,22 @@ public class MagicSquare {
      * @return whether the sum of each of the two main diagonals is equal to checkSum or not.
      */
     public static boolean magicDiagonals(int[][] array2d, int checkSum) {
-
+        checkSum = calculateCheckSum(array2d);
+        int sum = 0;
+        for (int i = 0; i < array2d.length; i++){
+            sum += array2d[i][i];
+        }
+        if (sum != checkSum){
+            return false;
+        }
+        sum = 0;
+        for (int i = array2d.length - 1; i >= 0; i--){
+            sum += array2d[i][i];
+        }
+        if (sum != checkSum){
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -48,7 +87,11 @@ public class MagicSquare {
      * @return whether array2d is magic or not.
      */
     public static boolean isMagic(int[][] array2d) {
-
+        int checkSum = calculateCheckSum(array2d);
+        if (magicRows(array2d, checkSum) && magicColumns(array2d, checkSum) && magicDiagonals(array2d, checkSum)){
+            return true;
+        }
+        return false;
     }
 
 }
